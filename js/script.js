3cf38a62ab9f941e4b777106bd49208f80dd5021
Derek Johnson <derek64johnson@gmail.com>
2022-10-16 02:38:47 -0600

    html changes and single page site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+const mainContent = document.getElementById('mainContent');
+const CVContent = document.getElementById('CVContent');
+
+const homeBtn = document.getElementById('homeBtn');
+const CVBtn = document.getElementById('CVBtn');
+const CVBtnMobile = document.getElementById('CVBtnMobile');
+const projectsBtn = document.getElementById('projectsBtn');
+
+CVBtn.addEventListener('click', () => {
+  if (CVContent.classList.contains('hidden')) {
+    mainContent.classList.toggle('hidden');
+    CVContent.classList.toggle('hidden');
+  }
+});
+CVBtnMobile.addEventListener('click', () => {
+  if (CVContent.classList.contains('hidden')) {
+    mainContent.classList.toggle('hidden');
+    CVContent.classList.toggle('hidden');
+  }
+});
+homeBtn.addEventListener('click', () => {
+  if (mainContent.classList.contains('hidden')) {
+    mainContent.classList.toggle('hidden');
+    CVContent.classList.toggle('hidden');
+  }
+});
+
 const btn = document.getElementById('menu-btn');
 const nav = document.getElementById('menu');
 
