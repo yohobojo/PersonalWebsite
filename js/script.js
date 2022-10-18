f53753434534b4ba2d290f385370879826b066e7
yohobojo <47697720+yohobojo@users.noreply.github.com>
2022-10-17 20:53:32 -0600

    small html styling

--- js/script.js
+++ js/script.js
@@ -10,18 +10,21 @@ CVBtn.addEventListener('click', () => {
   if (CVContent.classList.contains('hidden')) {
     mainContent.classList.toggle('hidden');
     CVContent.classList.toggle('hidden');
+    CVBtn.classList.toggle('text-blue-700');
   }
 });
 CVBtnMobile.addEventListener('click', () => {
   if (CVContent.classList.contains('hidden')) {
     mainContent.classList.toggle('hidden');
     CVContent.classList.toggle('hidden');
+    CVBtn.classList.toggle('text-blue-700');
   }
 });
 homeBtn.addEventListener('click', () => {
   if (mainContent.classList.contains('hidden')) {
     mainContent.classList.toggle('hidden');
     CVContent.classList.toggle('hidden');
+    CVBtn.classList.toggle('text-blue-700');
   }
 });
 
@@ -29,6 +32,11 @@ const btn = document.getElementById('menu-btn');
 const nav = document.getElementById('menu');
 
 btn.addEventListener('click', () => {
+  btn.classList.toggle('open');
+  nav.classList.toggle('flex');
+  nav.classList.toggle('hidden');
+});
+CVBtnMobile.addEventListener('click', () => {
   btn.classList.toggle('open');
   nav.classList.toggle('flex');
   nav.classList.toggle('hidden');
